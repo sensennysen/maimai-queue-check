ccef6ca22b2f01ca3eaceb000c45c899df8da207
sensennysen <73576618+sensennysen@users.noreply.github.com>
2026-02-18 02:46:56 +0800

    change experimental feature message; changelogs

--- src/data/changelog.js
+++ src/data/changelog.js
@@ -1,5 +1,32 @@
 export const changelogData = [
   {
+    version: 'v1.6.0',
+    date: 'February 18, 2026',
+    changes: [
+      {
+        type: 'feature',
+        title: 'Experimental Features',
+        description: 'Added a new tab in the Profile Dropdown Menu which contains experimental features.',
+      },
+      {
+        type: 'feature',
+        title: 'mpqcheck Profile',
+        description: '[Experimental] Added a new tab in the Profile Dropdown Menu which contains your maimai Best 50 songs.',
+        footnote: 'This requires you to import your scores, which I have provided instructions on how to',
+      },
+      {
+        type: 'visual',
+        title: 'UI Improvements',
+        description: 'Used a new font family, better loading animation, and thematic issues being addressed',
+      },
+      {
+        type: 'optimization',
+        title: 'Code Optimization',
+        description: 'Generic code optimizatio and security hardening',
+      }
+    ]
+  },
+  {
     version: 'v1.5.0',
     date: 'February 14, 2026',
     changes: [
@@ -33,64 +60,6 @@ export const changelogData = [
         type: 'visual',
         title: 'Branch Short Names',
         description: 'Added short names for branches to make them easier to identify.',
-      },
-    ]
-  },
-  {
-    version: 'v1.4.2',
-    date: 'February 8, 2026',
-    changes: [
-      {
-        type: 'bugfix',
-        title: 'Queue Suggestions',
-        description: 'Fixed an issue where the queue suggestions are not suggesting recent players.',
-      },
-    ]
-  },
-   {
-    version: 'v1.4.1',
-    date: 'February 6, 2026',
-    changes: [
-      {
-        type: 'bugfix',
-        title: 'Querying Optimization',
-        description: 'Optimized the querying of queues to prevent the app from checking all queues from all branches.',
-      },
-      {
-        type: 'feature',
-        title: 'Report Form',
-        description: 'The Contact Us footer now redirects to report form.',
-      },
-    ]
-  },
-   {
-    version: 'v1.4.0',
-    date: 'February 6, 2026',
-    changes: [
-      {
-        type: 'feature',
-        title: 'Queue Function Improvements',
-        description: 'Implemented Solo Queue toggle; Input now suggest players that have their preferred branch set to the location and recent players on that day; Adds similarity detection to avoid double entries. (Thanks to Tear for the suggestions!)',
-      },
-      {
-        type: 'improvement',
-        title: 'Querying Improvements',
-        description: 'Cached branch queries and fixed an issue where queues from the previous day is being carried over to the next day',
-      },
-      {
-        type: 'feature',
-        title: 'Name Constraints',
-        description: 'Users can now have at most 10 characters in their display name and queue entries.',
-      },
-      {
-        type: 'visual',
-        title: 'UI Improvements',
-        description: 'General UI improvements',
-      },
-      {
-        type: 'feature',
-        title: 'View-Only Mode',
-        description: 'Added a view-only mode for users to easily see the information on the branch. (Thanks to GTDD for the suggestion!)',
       },
     ]
   }
