2074fb0b67f1df6800f17b4f29a7d5295e29e081
Bille <bille.lagarde.personal@gmail.com>
2026-02-21 11:57:22 +0800

    added changelogs

--- src/data/changelog.js
+++ src/data/changelog.js
@@ -1,5 +1,26 @@
 export const changelogData = [
   {
+    version: 'v1.7.3',
+    date: 'February 21, 2026',
+    changes: [
+      {
+        type: 'feature',
+        title: 'Custom Display Photo',
+        description: 'Added an upload feature for custom display photos.',
+      },
+      {
+        type: 'fix',
+        title: 'Favorite songs improvement',
+        description: 'Allows user to edit the comment on their favorite songs.',
+      },
+      {
+        type: 'feature',
+        title: 'Multi-select on playlist making',
+        description: 'Allows user to select multiple songs when making a playlist.',
+      }
+    ]
+  },
+  {
     version: 'v1.7.2',
     date: 'February 20, 2026',
     changes: [
@@ -86,68 +107,4 @@ export const changelogData = [
       },
     ]
   },
-  {
-    version: 'v1.6.0',
-    date: 'February 18, 2026',
-    changes: [
-      {
-        type: 'feature',
-        title: 'Experimental Features',
-        description: 'Added a new tab in the Profile Dropdown Menu which contains experimental features.',
-      },
-      {
-        type: 'feature',
-        title: 'mpqcheck Profile',
-        description: '[Experimental] Added a new tab in the Profile Dropdown Menu which contains your maimai Best 50 songs.',
-        footnote: 'This requires you to import your scores, which I have provided instructions on how to',
-      },
-      {
-        type: 'visual',
-        title: 'UI Improvements',
-        description: 'Used a new font family, better loading animation, and thematic issues being addressed',
-      },
-      {
-        type: 'optimization',
-        title: 'Code Optimization',
-        description: 'Generic code optimizatio and security hardening',
-      }
-    ]
-  },
-  {
-    version: 'v1.5.0',
-    date: 'February 14, 2026',
-    changes: [
-      {
-        type: 'hotfix',
-        title: 'Distance Badge',
-        description: 'Fixed an issue where the distance badge is not showing up.',
-      },
-      {
-        type: 'visual',
-        title: 'UI Improvements',
-        description: 'Added themes! You can select your preferred theme in the settings.',
-      },
-    ]
-  },
-  {
-    version: 'v1.4.3',
-    date: 'February 12, 2026',
-    changes: [
-      {
-        type: 'hotfix',
-        title: 'Notifications',
-        description: 'Notifs that are more than a week old won\'t show up on the list anymore.',
-      },
-      {
-        type: 'visual',
-        title: 'UI Improvements',
-        description: 'Added visual feedback for queue updates.',
-      },
-      {
-        type: 'visual',
-        title: 'Branch Short Names',
-        description: 'Added short names for branches to make them easier to identify.',
-      },
-    ]
-  }
 ];
